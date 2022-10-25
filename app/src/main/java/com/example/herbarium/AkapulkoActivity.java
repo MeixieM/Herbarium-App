b25c6d7cf26b5983b894e1ddbc8eb85c4c07dc37
Alex Fernan <mercadoalexfernan@gmail.com>
2022-10-25 22:12:14 +0800

    add akapulko Activity t

--- app/src/main/java/com/example/herbarium/AkapulkoActivity.java
+++ app/src/main/java/com/example/herbarium/AkapulkoActivity.java
@@ -1,27 +1,39 @@
 package com.example.herbarium;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.ImageView;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
-public class AkapulkoActivity extends AppCompatActivity {
+import com.example.herbarium.databinding.ActivityAkapulkoBinding;
 
+public class AkapulkoActivity extends AppCompatActivity {
+    TextView linkTextView;
+    private ActivityAkapulkoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_akapulko);
 
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        binding = ActivityAkapulkoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
 
 
-
+        linkTextView = findViewById(R.id.txtMore);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
 }
