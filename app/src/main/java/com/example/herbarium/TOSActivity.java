d7e46741471ac51c90ff45be903b396113803a1a
Alex Fernan <mercadoalexfernan@gmail.com>
2022-10-29 16:32:40 +0800

    added TOS

--- app/src/main/java/com/example/herbarium/TOSActivity.java
+++ app/src/main/java/com/example/herbarium/TOSActivity.java
@@ -1,14 +1,34 @@
 package com.example.herbarium;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.herbarium.databinding.ActivityTosactivityBinding;
+
 public class TOSActivity extends AppCompatActivity {
+
+    private ActivityTosactivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tosactivity);
+
+        binding = ActivityTosactivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+
     }
 }
