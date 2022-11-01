561141ea5a0f6b7a4cae36c3fd9ce656cce93b4f
Alex Fernan <mercadoalexfernan@gmail.com>
2022-11-01 21:21:41 +0800

    add Guide Activity

---
+++ app/src/main/java/com/example/herbarium/GuideActivity.java
@@ -0,0 +1,34 @@
+package com.example.herbarium;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.herbarium.databinding.ActivityGuideBinding;
+
+public class GuideActivity extends AppCompatActivity {
+
+    private ActivityGuideBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityGuideBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+
+    }
+}
